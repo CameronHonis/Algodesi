4846e7042e74efca51137918924199a657c55beb
Kevin Lam <61953680+KevinLam2980@users.noreply.github.com>
2021-01-20 01:15:20 -0800

    some animations for selectionBar

--- src/components/SelectionBar.tsx
+++ src/components/SelectionBar.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
+import ItemSelection from './ItemSelection'
 
 type MenuState = {
   isOpen: boolean,
@@ -11,6 +12,100 @@ const initMenuState: MenuState = {
   isNodeType: false,
   isListType: false,
 }
+
+const nodeMenuItems: { itemName: string, className: string }[] = [
+  {
+    itemName: 'General Tree',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Binary Tree',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Binary Search Tree',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Trie',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'AVL Tree',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Red-black tree',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Splay Tree',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Treap',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'B-tree',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Acyclic Graph',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Cyclic Graph',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Weighted Graph',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Unweighted Graph',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Undirected Graph',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Directed Graph',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Simple Linked List',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Doubly Linked List',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Circular Linked List',
+    className: 'itemSelection'
+  },
+]
+
+const arrayMenuItems: { itemName: string, className: string }[] = [
+  {
+    itemName: 'Array',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Stack',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Set',
+    className: 'itemSelection'
+  },
+  {
+    itemName: 'Queue',
+    className: 'itemSelection'
+  },
+]
 
 const SelectionBar: React.FC = () => {
   const [menu, setMenu] = useState(initMenuState)
@@ -31,79 +126,90 @@ const SelectionBar: React.FC = () => {
     setMenu(initMenuState)
   }
 
-  if (!menu.isOpen) {
-    return (
-      <div id='selectionBar'>
-        <div
-          onClick={e => openDiv(e)}
-          id='nodeType'
-        >Graph Node</div>
-        <div
-          onClick={e => openDiv(e)}
-          id='arrayType'
-        >List item</div>
-        <div><p>pencil</p></div>
-        <div><p>eraser</p></div>
-        <div><p>input</p></div>
-      </div>
-    )
-  } else {
-    return (
-      <div className='itemMenu'>
-        <div onClick={e => closeDiv(e)}>x</div>
-        {menu.isNodeType ?
+  return (
+    <div className='itemMenu'>
+      {menu.isNodeType ?
+        <>
+          <div onClick={e => closeDiv(e)}>x</div>
+          <div className='itemSelection'><p>General Tree</p></div>
+          <div className='itemSelection'><p>Binary Search Tree</p></div>
+          <div className='itemSelection'><p>Trie</p></div>
+          <div className='itemSelection'><p>AVL Tree</p></div>
+          <div className='itemSelection'><p>Splay Tree</p></div>
+          <div className='itemSelection'><p>Treap</p></div>
+          <div className='itemSelection'><p>B-tree</p></div>
+          <div className='itemSelection'><p>Acyclic Graph</p></div>
+          <div className='itemSelection'><p>Cyclic Graph</p></div>
+          <div className='itemSelection'><p>Weighted Graph</p></div>
+          <div className='itemSelection'><p>Unweighted Graph</p></div>
+          <div className='itemSelection'><p>Undirected Graph</p></div>
+          <div className='itemSelection'><p>Directed Graph</p></div>
+          <div className='itemSelection'><p>Simple Linked List</p></div>
+          <div className='itemSelection'><p>Doubly Linked List</p></div>
+          <div className='itemSelection'><p>Circular Linked List</p></div>
+        </>
+        :
+        menu.isListType ?
           <>
-            <div><p>General Tree</p></div>
-            <div><p>Binary Tree</p></div>
-            <div><p>Binary Search Tree</p></div>
-            <div><p>Trie</p></div>
-            <div><p>AVL Tree</p></div>
-            <div><p>Red-black tree</p></div>
-            <div><p>Splay Tree</p></div>
-            <div><p>Treap</p></div>
-            <div><p>B-tree</p></div>
-            <div><p>Acyclic Graph</p></div> 
-            <div><p>Cyclic Graph</p></div> 
-            <div><p>Weighted Graph</p></div> 
-            <div><p>Unweighted Graph</p></div> 
-            <div><p>Undirected Graph</p></div> 
-            <div><p>Directed Graph</p></div>
-            <div><p>Simple Linked List</p></div>
-            <div><p>Doubly Linked List</p></div>
-            <div><p>Circular Linked List</p></div>
+            <div onClick={e => closeDiv(e)}>x</div>
+            <div className='itemSelection'><p>Array</p></div>
+            <div className='itemSelection'><p>Set</p></div>
+            <div className='itemSelection'><p>Stack</p></div>
+            <div className='itemSelection'><p>Queue</p></div>
           </>
           :
-          menu.isListType ?
-            <>
-              <div><p>Array</p></div>
-              <div><p>Set</p></div>
-              <div><p>Stack</p></div>
-              <div><p>Queue</p></div>
-            </>
-            : null
-        }
-      </div>
-    )
-  }
+          <>
+            <div
+              onClick={e => openDiv(e)}
+              id='nodeType'
+              className='mainSelection'
+            >Graph Node</div>
+            <div
+              onClick={e => openDiv(e)}
+              id='arrayType'
+              className='mainSelection'
+            >List item</div>
+            <div className='mainSelection'><p>pencil</p></div>
+            <div className='mainSelection'><p>eraser</p></div>
+            <div className='mainSelection'><p>input</p></div>
+          </>
+      }
+    </div>
+  )
+
+
   // return (
-  //   <div id='selectionBar'>
-  //     {!openMenu ?
-  //       <>
-  //         <div onClick={e => openDiv(e)}>node</div>
-  //         <div onClick={e => openDiv(e)}>list</div>
-  //         <div>pensil</div>
-  //         <div>eraser</div>
-  //         <div>input</div>
-  //       </>
+  //   <div className='itemMenu'>
+  //     <div onClick={e => closeDiv(e)}>x</div>
+  //     {menu.isNodeType ?
+  //       nodeMenuItems.map(item => {
+  //         return <ItemSelection itemName={item.itemName} className={item.className}/>
+  //       })
   //       :
-  //       <>
-  //           <div onClick={e => closeDiv(e)}>x</div>
-  //           <div>Binary Search Tree</div>
-  //           <div>Trie</div>
-  //       </>
-  //       }
-  //       </div> 
-  //   )
+  //       menu.isListType ?
+  //       arrayMenuItems.map(item => {
+  //         return <ItemSelection itemName={item.itemName} className={item.className}/>
+  //       })
+  //         :
+  //         <>
+  //           <div
+  //             onClick={e => openDiv(e)}
+  //             id='nodeType'
+  //             className='mainSelection'
+  //           >Graph Node</div>
+  //           <div
+  //             onClick={e => openDiv(e)}
+  //             id='arrayType'
+  //             className='mainSelection'
+  //           >List item</div>
+  //           <div className='mainSelection'><p>pencil</p></div>
+  //           <div className='mainSelection'><p>eraser</p></div>
+  //           <div className='mainSelection'><p>input</p></div>
+  //         </>
+  //     }
+  //   </div>
+  // )
+
 }
 
 export default SelectionBar
