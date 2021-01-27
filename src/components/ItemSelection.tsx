d8f18b18a5a32d77980467d02069a934fba6e4fd
Kevin Lam <61953680+KevinLam2980@users.noreply.github.com>
2021-01-26 23:40:33 -0800

    center text in each item

--- src/components/ItemSelection.tsx
+++ src/components/ItemSelection.tsx
@@ -19,13 +19,17 @@ const ItemSelection: React.FC<ItemSelectionProps> = (props) => {
         e.stopPropagation()
         if (e.target.parentElement.id === 'Graph') {
           refs.current.selected = 'NODE'
+          const collection = itemsByGroup[refs.current.selected];
+          const newX = 200
+          const newY = (collection.length * 50) + 25
+          setBarRefs(BarRefsAction.SET_BAR_TARGET_SIZE, { size: new V2(newX, newY) });
         } else if (e.target.parentElement.id === 'Array') {
           refs.current.selected = 'LIST'
+          const collection = itemsByGroup[refs.current.selected];
+          const newX = 200
+          const newY = (collection.length * 50) + 25
+          setBarRefs(BarRefsAction.SET_BAR_TARGET_SIZE, { size: new V2(newX, newY) });
         }
-        const collection = itemsByGroup[refs.current.selected];
-        const newX = 200
-        const newY = (collection.length * 50) + 25
-        setBarRefs(BarRefsAction.SET_BAR_TARGET_SIZE, { size: new V2(newX, newY) });
     }
   }
 
